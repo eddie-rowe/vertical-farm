f37a86493631159f19a7f683fbfa806d36abf943
eddie-rowe <74205376+eddie-rowe@users.noreply.github.com>
2025-05-18 20:35:31 -0500

    update

--- backend/main.py
+++ backend/main.py
@@ -13,9 +13,11 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://frontend:3000",
     "http://localhost:8080",
     "http://192.168.0.111:3000",
     "http://192.168.0.111:8080",
+    "http://vertical-farm.goodgoodgreens.org",
     "https://vertical-farm.goodgoodgreens.org"
 ]
 
